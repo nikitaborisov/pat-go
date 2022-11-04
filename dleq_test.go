f2cd5c2eb201c4a1fb2cf030ab8398091d88bdb4
Nikita Borisov <nikita.borisov@cloudflare.com>
2022-11-04 11:25:24 -0500

    DLEQ implementation

---
+++ dleq_test.go
@@ -0,0 +1,55 @@
+package pat
+
+import (
+	"crypto/rand"
+	"testing"
+
+	"github.com/cloudflare/circl/group"
+)
+
+func TestDLEQ(t *testing.T) {
+
+	x := group.Ristretto255.RandomScalar(rand.Reader)
+	r := group.Ristretto255.RandomScalar(rand.Reader)
+
+	g := group.Ristretto255.Generator()
+	h := group.Ristretto255.HashToElement([]byte("second generator"), []byte("generator DST"))
+
+	gx := group.Ristretto255.NewElement()
+	gx.MulGen(x)
+
+	hr := group.Ristretto255.NewElement()
+	hr.Mul(h, r)
+
+	c := group.Ristretto255.NewElement()
+	c.Add(gx, hr)
+
+	A := []group.Element{gx, c}
+	B := [][]group.Element{
+		{g, nil},
+		{g, h},
+	}
+	B2 := [][]group.Element{
+		{g, h},
+		{h, g},
+	}
+	svec := []group.Scalar{x, r}
+	err := VerifyProofParams(A, B, svec, group.Ristretto255)
+	if err != nil {
+		t.Error(err)
+	}
+	err = VerifyProofParams(A, B2, svec, group.Ristretto255)
+	if err == nil {
+		t.Errorf("Incorrect parameters should not verify")
+	}
+
+	p, err := ComputeProof(A, B, svec, group.Ristretto255, []byte("DLEQ test proof"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	result, err := VerifyProofCompact(p, A, B, group.Ristretto255, []byte("DLEQ test proof"))
+	if !result {
+		t.Errorf("Proof verification failed: %s", err)
+	}
+}
